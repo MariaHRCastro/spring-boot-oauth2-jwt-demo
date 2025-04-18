447078f9347f51cfa8551c5951f5fe63e3e8730b
MariaHRCastro <mariahribeiro@protonmail.com>
2025-04-18 20:17:05 -0300

    inserção BCryptPassword

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$vOnAn4L4b7zpNF2V1QMgUeAo7ymhKLwQYXskYRE83Stb0NemX3xrG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$vOnAn4L4b7zpNF2V1QMgUeAo7ymhKLwQYXskYRE83Stb0NemX3xrG');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
